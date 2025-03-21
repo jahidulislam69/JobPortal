8b9d821e6c59490094c5d1eff9d8b43fc561b790
jahidulislam69 <jahidulhassa777@gmail.com>
2025-03-21 15:51:43 +0600

    Manage jobs and slight change o applications

--- Client/src/Pages/Applications.jsx
+++ Client/src/Pages/Applications.jsx
@@ -32,14 +32,14 @@ const Applications = () => {
             }
         </div>
         <h2 className='text=xl font-semibold mb-4'>Jobs Applied</h2>
-        <table className='min-w-ful bg-white border rounded-lg'>
+        <table className='min-w-full bg-white border rounded-lg'>
           <thead>
             <tr>
               <th className='py-3 px-4 border-b text-left'>Company</th>
               <th className='py-3 px-4 border-b text-left '>Job Title</th>
               <th className='py-3 px-4 border-b text-left max-sm:hidden'>Location</th>
               <th className='py-3 px-4 border-b text-left max-sm:hidden'>Date</th>
-              <th className='py-3 px-4 border-b text-left'>Status</th>
+              <th className='py-3 px-4 border-b text-center'>Status</th>
             </tr>
           </thead>
           <tbody>
@@ -52,7 +52,7 @@ const Applications = () => {
                 <td className='py-2 px-4 border-b'>{job.title}</td>
                 <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
                 <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                <td className='py-2 px-4 border-b'><span className={`${job.status === 'Accepted' ? 'bg-green-100': job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>{job.status}</span></td>
+                <td className='py-2 px-4 border-b text-center'><span className={`${job.status === 'Accepted' ? 'bg-green-100': job.status === 'Rejected' ? 'bg-red-100' : 'bg-blue-100'} px-4 py-1.5 rounded`}>{job.status}</span></td>
               </tr>
             ):(null))}
           </tbody>
